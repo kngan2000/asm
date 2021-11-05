0a182d15118a0ce420b0f064050a59bd3c35e9f7
kngan2000 <91657071+kngan2000@users.noreply.github.com>
2021-11-05 11:18:26 +0700

    hi

--- index.php
+++ index.php
@@ -26,6 +26,31 @@ include_once("connection.php");
 ?>
 
 <body>
+	<header id="header"><!--header-->
+		<div class="header_top"><!--header_top-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6">
+						<div class="contactinfo">
+							<ul class="nav nav-pills">
+								<li><a href="#"><i class="fa fa-phone"></i> (+84) 220 8604</a></li>
+								<li><a href="#"><i class="fa fa-envelope"></i> kieungann0711@gmail.com</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="social-icons pull-right">
+							<ul class="nav navbar-nav">
+								<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+								<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+								<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div><!--/header_top-->		
 		<div class="header-middle"><!--header-middle-->
 			<div class="container">
