6ae03431d35f725e8a6a285ba680d6200c6951a0
kngan2000 <91657071+kngan2000@users.noreply.github.com>
2021-11-07 11:08:23 +0700

    1

--- index.php
+++ index.php
@@ -6,19 +6,6 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <title>ATN COMPANY</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/prettyPhoto.css" rel="stylesheet">
-    <link href="css/price-range.css" rel="stylesheet">
-    <link href="css/animate.css" rel="stylesheet">
-	<link href="css/main.css" rel="stylesheet">
-	<link href="css/responsive.css" rel="stylesheet">
-	     
-    <link rel="shortcut icon" href="images/ico/favico.icon">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
 </head><!--/head-->
 <?php
 Session_Start();
