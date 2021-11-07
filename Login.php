7950d4eb529e67ebbb0b1ddd778e6b8c6bc36f8d
kngan2000 <91657071+kngan2000@users.noreply.github.com>
2021-11-07 22:52:47 +0700

    kkkk

--- Login.php
+++ Login.php
@@ -1,47 +1,64 @@
+    
 <?php
-    session_start();
-    include('connection.php');
-    if(isset($_POST['submit'])){
-        $username = $_POST['username'];
-        $password = md5($_POST['password']);
-        $sql = "SELECT * FROM user WHERE username='".$username."' AND 
-        password='".$password."' LIMIT 1";
-        $row = pg_fetch_array($res, NULL, PGSQL_ASSOC);
-        $count = pg_num_rows($row);
-        if($count >0){
-            $_SESSION['submit'] = $username;
-            header("Location:index.php");
-        }else{
-            echo '<script>alert("Incorrect account or password, please re-enter!")</script>';
-            header("Location:login.php");
-        }
+
+//Xử lý đăng nhập
+if (isset($_POST['dangnhap'])) {
+    //Kết nối tới database
+    include_once('connect.php');
+
+    //Lấy dữ liệu nhập vào
+    $username = addslashes($_POST['txtUserName']);
+    $password = addslashes($_POST['txtPassword']);
+
+    //Kiểm tra tên đăng nhập có tồn tại không
+    $result = pg_query($conn, "SELECT user_name, password,state FROM public.user WHERE user_name='{$username}'");
+    if (pg_num_rows($result) == 0) {
+        echo "Tên đăng nhập này không tồn tại. Vui lòng kiểm tra lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
+        exit;
     }
+
+    //Lấy mật khẩu trong database ra
+    $row = pg_fetch_array($result);
+
+    //So sánh 2 mật khẩu có trùng khớp hay không
+    if ($password != $row['password']) {
+        echo "Mật khẩu không đúng. Vui lòng nhập lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
+        exit;
+    }
+
+    if (pg_num_rows($result) == 1) {
+        $_SESSION["user_name"] = $username;
+        $_SESSION["admin"] = $row['state'];
+        echo"thanh cong";
+        echo '<meta http-equiv="refresh" content="0;URL=index.php"/>';
+    } else {
+        echo "You loged in fail!";
+    }
+}
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Log in</title>
+    <title>Login</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 </head>
+
 <body>
-    <h3>Log in</h3>
-    <table>
-        <tr>
-            <td>Username: </td>
-            <td><input type="text" name="username"></td>
-        </tr>
-        <tr>
-            <td>Password: </td>
-            <td><input type="password" name="password"></td>
-        </tr>
-        <tr>
-            <td colspan="2">
-                <button type="submit" name="submit">Login</button>
-                <button type="reset">Reset</button>
-            </td>
-        </tr>
-    </table>
-</form>
+    <div class="container">
+        <form action="" method="POST">
+            <div class="form-group">
+                <label for="exampleInput">User name</label>
+                <input type="text" name="txtUserName" class="form-control" id="exampleInput" aria-describedby="" placeholder="User Name">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" name="txtPassword" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            </div>
+            <button type="submit" class="btn btn-primary" name="dangnhap" value="Submit">Submit</button>
+
+        </form>
+    </div>
 </body>
+
 </html>
--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
-    //$conn = pg_connect("postgres://enesxkbnipglfn:ab5ea2e4b66767be64939974975f8a8e408c547b2d474887e57875666e04a13f@ec2-3-220-90-40.compute-1.amazonaws.com:5432/d5kcq00ts60umq");
-    $conn = pg_connect("asm://postgres:kieungan0711@localhost:5432/postgres");
+    $conn = pg_connect("postgres://enesxkbnipglfn:ab5ea2e4b66767be64939974975f8a8e408c547b2d474887e57875666e04a13f@ec2-3-220-90-40.compute-1.amazonaws.com:5432/d5kcq00ts60umq");
+    //$conn = pg_connect("postgresql://postgres:kieungan0711@localhost:5432/postgres");
     if(!$conn){
         die("Can not connect database");
     } 
