23793d41dfdd0521b6af613ce358bad5d7dee56b
kngan2000 <91657071+kngan2000@users.noreply.github.com>
2021-11-07 23:34:39 +0700

    i

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = pg_connect("postgres://enesxkbnipglfn:ab5ea2e4b66767be64939974975f8a8e408c547b2d474887e57875666e04a13f@ec2-3-220-90-40.compute-1.amazonaws.com:5432/d5kcq00ts60umq");
+    $conn = pg_connect("postgres://qnjmevmozucnte:aff42e5b5cbc098fcb550be01c5dd70af7dd5a0b16f03f50f5a28aaa5aca3e44@ec2-54-147-76-191.compute-1.amazonaws.com:5432/d1av1lf8sjopf7");
     //$conn = pg_connect("postgresql://postgres:kieungan0711@localhost:5432/postgres");
     if(!$conn){
         die("Can not connect database");
