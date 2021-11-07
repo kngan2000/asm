debfa9056a5cff36ca427b534dfe037ae10bf122
kngan2000 <91657071+kngan2000@users.noreply.github.com>
2021-11-07 14:02:53 +0700

    hi

--- Login.php
+++ Login.php
@@ -1,66 +1,28 @@
-<?php
-    
-    if(isset($_POST['btnLogin']))
-    {
-        $us = $_POST['txtUsername'];
-        $pa = $_POST['txtPass'];
-        $err = "";
-        if($us =="")
-        {
-            $err .= "Enter username, please<br/>";
-        }
-        if($pa =="")
-        {
-            $err .= "Enter password, please<br/>";
-        }
-        if($err !="")
-        {
-            echo $err;
-        }
-        else
-        {
-            include_once("connection.php");
-            $pass = md5($pa);
-            $res = mysqli_query($conn, "SELECT username, password, state FROM customer WHERE Username='$us' AND Password='$pass'")
-                    or die(mysqli_error($conn));
-            $row = mysqli_fetch_array($res, MYSQLI_ASSOC);
-            if(mysqli_num_rows($res)==1)
-            {
-                $_SESSION["us"] = $us;
-                $_SESSION["admin"] = $row["state"];
-                echo '<meta http-equiv="refresh" content="0;URL=index.php" />';
-            }
-            else
-            {
-                echo "You login in fail";
-            }
-        }
-    }
-?>
-
-
-<h1>Login</h1>
-<form id="form1" name="form1" method="POST">
-<div class="row">
-    <div class="form-group">				    
-        <label for="txtUsername" class="col-sm-2 control-label">Username(*):  </label>
-		<div class="col-sm-12">
-		      <input type="text" name="txtUsername" id="txtUsername" class="form-control" placeholder="Username" value=""/>
-		</div>
-      </div>  
-      
-    <div class="form-group">
-		<label for="txtPass" class="col-sm-2 control-label">Password(*):  </label>			
-		<div class="col-sm-12">
-		      	<input type="password" name="txtPass" id="txtPass" class="form-control" placeholder="Password" value=""/>
-		</div>
-	</div> 
-    <div class="form-group1"> 
-        <div class="col-sm-2"></div>
-        <div class="col-sm-12">
-        	<input type="submit" name="btnLogin"  class="btn btn-primary" id="btnLogin" value="Login"/>
-            <input type="submit" name="btnCancel"  class="btn btn-primary" id="btnLogin" value="Cancel"/>
-		</div>  
-	</div>
- </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Log in</title>
+</head>
+<body>
+    <h3>Log in</h3>
+    <table>
+        <tr>
+            <td>Username: </td>
+            <td><input type="text" name="username"></td>
+        </tr>
+        <tr>
+            <td>Password: </td>
+            <td><input type="password" name="password"></td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <button type="submit" name="submit">Login</button>
+                <button type="reset">Reset</button>
+            </td>
+        </tr>
+    </table>
 </form>
+</body>
+</html>
