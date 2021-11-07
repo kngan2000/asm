99023c40a2db88f20accf4a3bf5fb27bd68e4d77
kngan2000 <91657071+kngan2000@users.noreply.github.com>
2021-11-07 21:00:23 +0700

    2

--- Login.php
+++ Login.php
@@ -1,3 +1,40 @@
+<?php
+
+//Xử lý đăng nhập
+if (isset($_POST['submit'])) {
+    //Kết nối tới database
+    include_once('connect.php');
+
+    //Lấy dữ liệu nhập vào
+    $username = addslashes($_POST['username']);
+    $password = addslashes($_POST['password']);
+
+    //Kiểm tra tên đăng nhập có tồn tại không
+    $result = pg_query($conn, "SELECT username, password FROM public.user WHERE username='{$username}'");
+    if (pg_num_rows($result) == 0) {
+        echo "Tên đăng nhập này không tồn tại. Vui lòng kiểm tra lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
+        exit;
+    }
+
+    //Lấy mật khẩu trong database ra
+    $row = pg_fetch_array($result);
+
+    //So sánh 2 mật khẩu có trùng khớp hay không
+    if ($password != $row['password']) {
+        echo "Mật khẩu không đúng. Vui lòng nhập lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
+        exit;
+    }
+
+    if (pg_num_rows($result) == 1) {
+        $_SESSION["username"] = $username;
+        //$_SESSION["admin"] = $row['state'];//
+        echo"thanh cong";
+        echo '<meta http-equiv="refresh" content="0;URL=index.php"/>';
+    } else {
+        echo "You loged in fail!";
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
