9f12c0a1abb76b45aee59844b4a5131b7e02d6e8
kngan2000 <91657071+kngan2000@users.noreply.github.com>
2021-11-07 12:38:20 +0700

    1

--- Register.php
+++ Register.php
@@ -3,10 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
 </head>
 <body>
-    
+    <h3>Đăng kí thành viên</h3>
+    <form action="register_submit.php" method="POST">
+        <table>
+            <tr>
+                <td>Username: </td>
+                <td><input type="text" name="username"></td>
+            </tr>
+            <tr>
+                <td>Password: </td>
+                <td><input type="password" name="password"></td>
+            </tr>
+            <tr>
+                <td>Confirm Password: </td>
+                <td><input type="password" name="repassword"></td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <button type="Submit" name="submit">Đăng ký</button>
+                    <button type="reset">Làm mới</button>
+
 </body>
 </html>
